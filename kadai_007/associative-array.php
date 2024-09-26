86b401eeba698cb1e71fbb048c63406e796885d0
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-09-26 15:50:55 +0900

    kadai_007をアップ

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+      <meta charset="UTF-8">
+      <title>kadai_007</title>
+</head>
+
+<body>
+      <p>
+          <?php
+          //連想配列に値を代入する
+          $vegitable_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+          //onionデータを出力する
+          print_r($vegitable_data);
+          ?>
+      </p>
+</body>
+
+</html>
