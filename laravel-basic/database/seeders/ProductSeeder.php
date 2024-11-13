d8e83cbd661991ede9f9c4fbdcdd8c9e29e508c5
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-13 16:28:55 +0900

    ファクトリの作成＆編集、シーダークラスの作成

--- laravel-basic/database/seeders/ProductSeeder.php
+++ laravel-basic/database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('products')->insert([
+            'product_name' => 'ノート５冊セット',
+            'price' => 600,
+            'created_at' => '2023-06-01 00:00:00',
+            'updated_at' => '2023-06-01 00:00:00',
+            'vendor_code' => 1111,
+        ]);
     }
 }
--- laravel-basic/database/seeders/VendorSeeder.php
+++ laravel-basic/database/seeders/VendorSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class シーダークラス名 extends Seeder
+class VendorSeeder extends Seeder
 {
     /**
      * Run the database seeds.
