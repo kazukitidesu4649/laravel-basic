dd7afad884bd7e02fc63ff659e1328ae33d6d63e
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-13 15:17:44 +0900

    vendorsテーブルの作成

--- laravel-basic/routes/web.php
+++ laravel-basic/routes/web.php
@@ -23,3 +23,5 @@ Route::get('/', function () {
 Route::get('/hello', [HelloController::class, 'index']);
 
 Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
