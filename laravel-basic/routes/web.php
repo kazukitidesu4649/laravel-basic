3034859191c0a40703a6ca74a06b59c99c7c6b64
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-13 17:48:01 +0900

    RequestControllerのビューの作成

--- laravel-basic/routes/web.php
+++ laravel-basic/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\VendorController;
+use App\Http\Controllers\RequestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,7 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 
 Route::get('/vendors/{id}', [VendorController::class, 'show']);
+
+Route::get('/requests/create', [RequestController::class, 'create']);
+
+Route::get('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
