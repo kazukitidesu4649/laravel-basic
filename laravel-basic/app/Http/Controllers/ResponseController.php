3118d4bac462088d6aaabf0b92eceeadecbc6f77
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-13 23:55:11 +0900

    ResponseControllerを編集

--- laravel-basic/app/Http/Controllers/ResponseController.php
+++ laravel-basic/app/Http/Controllers/ResponseController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class ResponseController extends Controller
 {
-    //
+    public function index() {
+        return response()->json(['id' => 1, 'email' => 'taro.samurai@example.com']);
+    }
 }
