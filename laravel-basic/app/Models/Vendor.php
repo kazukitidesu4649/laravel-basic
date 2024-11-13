c23931ca91760105ca479d0fb640f0f57b1503bb
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-13 15:35:06 +0900

    Vendor, Productモデルの編集

--- laravel-basic/app/Models/Vendor.php
+++ laravel-basic/app/Models/Vendor.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Vendor extends Model
 {
     use HasFactory;
+
+    // １つの仕入れ先は複数の商品を持てる
+    public function products() {
+        return $this->hasMany(Product::class, 'vendor_code', 'vendor_code');
+    }
 }
