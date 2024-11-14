75956df42546e31468b4e5c1de13833385d2a79f
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-14 16:23:13 +0900

    UserSeederの編集

--- laravel-basic/database/seeders/UserSeeder.php
+++ laravel-basic/database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name' => '侍 花子',
+            'email' => 'hanako.samurai@example.com',
+            'password' => 'password',
+            'created_at' => '2023-01-01 00:00:00',
+            'updated_at' => '2023-01-01 00:00:00',
+        ]);
     }
 }
