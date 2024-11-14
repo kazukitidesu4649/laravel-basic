a94e21fcff96276b5374b1b26097936fcf7e6cff
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-14 14:30:03 +0900

    ProductControllerの編集

--- laravel-basic/app/Http/Requests/ProductStoreRequest.php
+++ laravel-basic/app/Http/Requests/ProductStoreRequest.php
@@ -11,7 +11,7 @@ class ProductStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class ProductStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'product_name' => 'required|max:255',
+            'price' => 'required|integer|min:1',
+            'vendor_code' => 'exists:vendors,vendor_code',
         ];
     }
 }
