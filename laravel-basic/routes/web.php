22c85b26ac95330d21cd311434d66b52838f5e7b
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-19 10:53:21 +0900

    CookieControllerを作成

--- laravel-basic/routes/web.php
+++ laravel-basic/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\RequestController;
 use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\SignInController;
 
 /*
 |--------------------------------------------------------------------------
@@ -44,3 +45,7 @@ Route::get('/requests/create', [RequestController::class, 'create']);
 Route::get('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
 
 Route::get('/responses', [ResponseController::class, 'index']);
+
+Route::get('/sign-in',[SignInController::class, 'create']);
+
+Route::post('/sign-in', [SignInController::class, 'store'])->name('sign-in.store');
