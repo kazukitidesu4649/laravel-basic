952b84ab0e783b497182355242653ec943e15018
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-19 11:26:50 +0900

    CookieControllerの編集,ルーティングを編集

--- laravel-basic/app/Http/Controllers/CookieController.php
+++ laravel-basic/app/Http/Controllers/CookieController.php
@@ -8,5 +8,47 @@ use App\Models\Product;
 
 class CookieController extends Controller
 {
+    // indexメソッドの作成
+    public function index() {
+        // クッキーから'product_id'キーの値を取得する
+        $product_id = Cookie::get('product_id');
+
+        // Productsテーブルからクッキーで取得したidのレコードを取得
+        $product = Product::find($product_id);
+
+        // views/Cookie/index.blade.php に compact関数を使用して'product'を引数に$productを配列として 渡す。
+        return view('Cookies.index', compact('product'));
+    }
+
+    // createメソッドの作成
+    public function create() {
+        // productsテーブルの全てのレコードを$productsに格納
+        $products = Product::all();
+
+        // views/Cookies/create.php に compact関数を使用して'products'引数に$productsを配列として 渡す。
+        return view('cookies.create', compact('products'));
+    }
+
+    // storeメソッドを作成し、HTTPリクエストデータを処理する。
+    public function store(Request $request){
+        // 
+        $request->validate([
+            'product_id' => 'required|exists:products,id'
+        ]);
+
+        // キー名が'product_id'、値が商品IDのデータをクッキーに設定する(60分有効)
+        Cookie::queue('product_id',$request->input('product_id'),60);
+
+        // HTTPレスポンスと同時にクッキーが送信される
+        return redirect('/cookies');
+    }
+    
     //
+    public function destroy() {
+        //
+        Cookie::queue(Cookie::forget('product_id'));
+
+        //
+        return redirect('/cookies');
+    }
 }
