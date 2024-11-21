fc861fe6b2ad3a189fcdd689c343f584b168cdbc
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-22 00:38:09 +0900

    tests/Feature/ProductTest.php テストメソッドの定義

--- laravel-basic/tests/Feature/ProductTest.php
+++ laravel-basic/tests/Feature/ProductTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Vendor;
 
 class ProductTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      */
@@ -16,5 +18,13 @@ class ProductTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_product_can_be_created() {
+        // 予め仕入先のデータを用意しておく
+        $vendor = new Vendor();
+        $vendor->vendor_code = 1111;
+        $vendor->vendor_name = 'SAMURAI商店';
+        $vendor->save();
     }
 }
